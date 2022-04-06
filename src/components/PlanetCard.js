ffc3e10e7b78a7dc47fa84c81036ecbea39ee407
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-04-06 18:37:27 -0300

    req 05 - implementa componente PlanetCard

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,31 +1,43 @@
-// 1. Crie um componente chamado PlanetCard dentro da pasta src/components.
+import React from 'react';
+import PropTypes from 'prop-types';
 
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
+
+// 1. Crie um componente chamado PlanetCard dentro da pasta src/components.
 // 2. O componente PlanetCard deve receber duas props: uma chamada planetName
 // e outra chamada planetImage.
-
 // 3. O componente PlanetCard deve ter uma div que envolva todo seu conteúdo e
 // que tenha o atributo data-testid="planet-card".
-
 // 4. O componente PlanetCard deve renderizar o texto recebido pela prop planetName.
 // Sugerimos a utilização de tags de Conteúdo de Fluxo, como <p>, que deve conter
 // o atributo data-testid="planet-name".
-
 // 5. O componente PlanetCard deve renderizar uma imagem que tenha o atributo src
 // com o valor recebido pela prop planetImage.
-
 // 6. Além do atributo src, a imagem renderizada deve ter o atributo alt com o
 // texto Planeta {planetName}, onde {planetName} é o valor recebido pela prop planetName.
-
 // O que será verificado
 // Será validado se o componente <PlanetCard /> é renderizado.
-
 // Será validado se o componente <PlanetCard /> possui uma div com o
 // atributo data-testid="planet-card".
-
 // Será validado se é renderizado o texto recebido pela prop planetName.
-
 // Será validado se é renderizada uma imagem com o atributo src com o mesmo valor
 // recebido pela prop planetImage.
-
 // Será validado se, além do atributo src, a imagem renderizada possui o atributo alt
 // com o texto Planeta {planetName}, onde {planetName} é o valor recebido pela prop planetName.
