0921bcc5cca522b9388028649bb12529c58b2b10
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-04-06 19:02:39 -0300

    req 06 - renderiza PlanetCard em SolarSystem

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,8 +6,8 @@ class PlanetCard extends React.Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">{planetName}</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{planetName}</p>
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Title from './Title';
-// // import planets from '../data/planets'; // req 6
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((elPlanet) => (
+          <PlanetCard
+            planetName={ elPlanet.name }
+            planetImage={ elPlanet.image }
+            key={ elPlanet.name }
+          />
+        ))}
       </div>
     );
   }
